f16832fd29515193ec3543f3a41c6dcb16a1fc8c
Alexandr Pekel <spekel2003@gmail.com>
2025-06-10 10:54:46 +0300

    Add usage examples for creational design patterns
    Expanded Program.cs to demonstrate Singleton, Prototype, Builder, Abstract Factory, and Factory Method patterns. The main method now includes instantiations and method calls for each pattern, providing a comprehensive example of their usage.

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Creational.Singleton;
+using Creational.Prototype;
+using Creational.Builder;
+using Creational.AbstractFactory.Factories;
+using Creational.AbstractFactory.Interfaces;
+using Creational.FactoryMethod.Interfaces;
+using Creational.FactoryMethod.Factories;
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        // Singleton pattern
+        var config = ConfigurationManagerSingleton<DeviceInfoProvider>.Instance;
+        var deviceModel = config.GetDeviceModel();
+
+        // Prototype pattern
+        var originalAndroidModel = new AndroidModel(1, "Robot A", "Model A", "Android A", "Cpu A", "Gpu A");
+        var clonedAndroidModel = new AndroidModel(2, "Robot B", "Model B", "Android B", "Cpu B", "Gpu B");
+
+        var originalRobot = new AndroidRobot(originalAndroidModel);
+        originalRobot.ToString();
+
+        var clonedRobot = originalRobot.Clone();
+        clonedRobot.ToString();
+
+        clonedRobot.SetData(clonedAndroidModel);
+        clonedRobot.ToString();
+
+
+        // Builder pattern
+        var appSettings = new AppSettingsBuilder()
+            .SetResolution(1920, 1080)
+            .SetFullscreen(true)
+            .SetVolume(50)
+            .SetBrightness(50)
+            .SetLanguage("English")
+            .Build();
+
+        var appSettings1 = new AppSettingsBuilder();
+        var appSettings2 = new SettingsDirector().SetDefault(appSettings1);
+
+
+        // Abstract Factory pattern
+        IOsFactory osFactory = deviceModel.OperatingSystem.Contains("windows")
+            ? new WindowsFactory()
+            : new AppleFactory();
+
+        osFactory.GetFileSystem().CreateDirectory("/path");
+        osFactory.GetFileSystem().DeleteDirectory("/path");
+        osFactory.GetAntivirusSystem().CheckIfIsSave("/path");
+
+        //Factory Method pattern
+        ApplicationCreatorBase appFactory = deviceModel.OperatingSystem.Contains("windows")
+        ? new MicrosoftAppFactory()
+        : new AppleAppFactory();
+
+        appFactory.CreateApp().InstallApp();
+        appFactory.CreateApp().UninstallApp();
+        
+
+
+
+
+
+
+
+    }
+}
+}
